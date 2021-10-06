2bb72032daed54f3e6abd32b2533f24608404075
Akash-Shekhawat <akash.shekhawat@tothenew.com>
2021-10-06 08:07:11 +0530

    exercise2-iii

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +20,28 @@ class MainActivity : AppCompatActivity() {
         next = findViewById<View>(R.id.btn_Next) as Button
 
 
-        next!!.setOnClickListener {
+        next?.setOnClickListener {
             startActivity(Intent(this@MainActivity, LoginActivity::class.java))
             finish()
+        }
+    }
+    fun fragmentChange(view: View){
+       var fragmant: Fragment
+
+       if(view== findViewById(R.id.button1)){
+           fragmant=FragmentOne()
+           val fm = supportFragmentManager
+           val ft: FragmentTransaction = fm.beginTransaction()
+           ft.replace(R.id.Frag1,fragmant)
+           ft.commit()
+
+       }
+        if(view== findViewById(R.id.button2)){
+            fragmant=FragmentTwo()
+            val fm = supportFragmentManager
+            val ft: FragmentTransaction = fm.beginTransaction()
+            ft.replace(R.id.Frag1,fragmant)
+            ft.commit()
         }
     }
 }
